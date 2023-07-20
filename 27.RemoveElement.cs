5a56daf4d88718553c8a80955ab01700fa9c0a84
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2023-07-20 12:10:34 +0300

    27. solved

---
+++ 27.RemoveElement.cs
@@ -0,0 +1,29 @@
+﻿namespace _27.RemoveElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 2, 3, 4, 5, 3 };
+            int newlength = RemoveElement(nums, 2);
+            Console.WriteLine(newlength);
+            for (int i = 0; i < newlength; i++)
+            {
+                Console.Write(nums[i]+" ");
+            }
+        }
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+        }
+    }
+}
