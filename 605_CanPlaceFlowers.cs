f9e6b5f052976f4586ead1bb9b62996ce06d19b9
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2023-07-22 10:38:50 +0300

    605_solved

---
+++ 605_CanPlaceFlowers.cs
@@ -0,0 +1,31 @@
+﻿namespace _605_CanPlaceFlowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool test = CanPlaceFlowers(new int[] { 1, 0, 0}, 1);
+            Console.WriteLine(test);
+            
+        }
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            //[1,0,0,0,1] [0,0,0] [0,0,1,0,0]
+            int count = 0; 
+            for (int i = 0; i<flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    count++; 
+                    if (count == n)
+                    {
+                        return true;
+                    }
+                    i++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
