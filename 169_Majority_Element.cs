d112da993171a27272055f412abc201377d71b8e
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2023-07-24 10:20:13 +0300

    169 solved

---
+++ 169_Majority_Element.cs
@@ -0,0 +1,32 @@
+﻿namespace _169_MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MajorityElement(new int[] { 3, 2, 3 }));
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            int total = 0, num1 = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total = 0;
+                num1 = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        total++;
+                    }
+
+                }
+                if ((total) > nums.Length / 2)
+                {
+                    break;
+                }
+            }
+            return num1;
+        }
+    }
+}
