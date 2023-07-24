1121e47509295b9160a3c9d13dc8357988489be8
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2023-07-25 02:09:42 +0300

    345 solved

---
+++ 345_ReverseVowelsOfaString.cs
@@ -0,0 +1,44 @@
+﻿namespace _345_ReverseVowelsOfaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseVowels("Marge, let's \"went.\" I await news telegram."));
+        }
+        public static string ReverseVowels(string s)
+        {
+            string str = "aAeEuUiIoO";
+
+            int start = 0, end = s.Length - 1;
+
+            char[] chars = s.ToCharArray();
+
+
+            while (start < end)
+            {
+                if (str.Contains(chars[start]) && str.Contains(chars[end]))
+                {
+                    char temp = chars[start];
+                    chars[start] = chars[end];
+                    chars[end] = temp;
+                    start++;
+                    end--;
+                }
+                else
+                {
+                    if (!str.Contains(chars[start]))
+                    {
+                        start++;
+                    }
+                    if (!str.Contains(chars[end]))
+                    {
+                        end--;
+                    }
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
