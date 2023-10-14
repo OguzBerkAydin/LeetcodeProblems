a1ba37d499efb71d5e602c2c240208e77f785b6d
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2023-10-15 01:45:57 +0300

    .

---
+++ 392_IsSubsequence.cs
@@ -0,0 +1,31 @@
+﻿namespace _392_IsSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSubsequence("acb", "ahbgdc"));
+
+        }
+        public static bool IsSubsequence(string s, string t)
+        {
+            s = s.ToLower();
+            t = t.ToLower();
+
+            int sIndex = 0;
+            int tIndex = 0;
+
+            while (sIndex < s.Length && tIndex < t.Length)
+            {
+                if (s[sIndex] == t[tIndex])
+                {
+                    sIndex++;
+                }
+                tIndex++;
+            }
+
+            return sIndex == s.Length;
+        }
+    }
+
+}
