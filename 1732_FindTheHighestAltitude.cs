e6e8c7c0302f17fc5a82a66604f567bbfec52bd1
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2023-10-17 01:15:56 +0300

    .

---
+++ 1732_FindTheHighestAltitude.cs
@@ -0,0 +1,34 @@
+﻿namespace _1732_FindTheHighestAltitude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LargestAltitude(new int[] { 52, -91, 72 }));
+        }
+        public static int LargestAltitude(int[] gain)
+        {
+            if (gain.Length == 1 && gain[0] <= 0)
+            {
+                return 0;
+            }
+            else if (gain.Length == 1 && gain[0] >= 0)
+            {
+                return gain[0];
+            }
+
+            int maxAltitude = 0;
+            int checkAltitude = 0;
+
+            for (int i = 0; i < gain.Length; i++)
+            {
+                checkAltitude += gain[i];
+                if (checkAltitude > maxAltitude)
+                {
+                    maxAltitude = checkAltitude;
+                }
+            }
+            return maxAltitude;
+        }
+    }
+}
