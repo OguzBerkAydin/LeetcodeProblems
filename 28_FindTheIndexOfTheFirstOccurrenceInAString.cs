d9b237ce23607e8cb5a36ea268fb4814f5723229
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2023-11-03 02:34:08 +0300

    28. solved

---
+++ 28_FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,42 @@
+﻿namespace _28_FindTheIndexOfTheFirstOccurrenceInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrStr("sabutsad","sad"));
+            
+        }
+        public static int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) 
+            {
+                return 0;
+            }
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++) 
+            {
+                int matchIndex = i;
+                bool isMatch = true;
+
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[matchIndex] != needle[j])
+                    {
+                        isMatch = false;
+                        break; 
+                    }
+                    matchIndex++;
+                }
+
+                if (isMatch)
+                {
+                    return i; 
+                }
+            }
+
+            return -1; 
+        }
+        
+    }
+}
