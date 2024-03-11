74c298c02f7da0dc65cc91ef3ca9b6b795a22966
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-03-11 16:19:56 +0300

    two sum 2 solved with hashmap

---
+++ 167_TwoSumII-InputArrayIsSorted.cs
@@ -0,0 +1,27 @@
+﻿namespace _167._Two_Sum_II___Input_Array_Is_Sorted
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+		public int[] TwoSum(int[] numbers, int target)
+		{
+
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int remainder = target - numbers[i];
+				if (dict.ContainsKey(remainder))
+				{
+					return new int[] { dict[remainder], i + 1 };
+				}
+				dict[numbers[i]] = i + 1;
+
+			}
+			return null;
+
+		}
+	}
+}
