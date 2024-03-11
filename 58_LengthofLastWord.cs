abe9d7a3950dd12d215aa61b5891987ac483efe2
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-03-11 16:27:13 +0300

    #

---
+++ 58_LengthofLastWord.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace _58_LengthofLastWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+        public int LengthOfLastWord(string s)
+        {
+            int count = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    count++;
+
+                }
+                if (count > 0 && s[i] == ' ')
+                    return count;
+
+
+            }
+            return count;
+        }
+
+    }
+    
+}
