5e43c7dbbac37929d455390bb065b73682266ace
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-03-11 16:24:52 +0300

    #

---
+++ 35_SearchInsertPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace _35_SearchInsertPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] >= target)
+                    return i;
+            return nums.Length;
+        }
+        
+    }
+    
+}
