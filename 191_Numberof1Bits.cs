2111b3170c9c289aceaf5266ff4a48a0653bc60d
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-03-13 19:55:08 +0300

    #

---
+++ 191_Numberof1Bits.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace _191_Numberof1Bits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(MergeAlternately("asdasd", "fgh"));
+
+        }
+
+        public static int HammingWeight(uint n)
+        {
+            int counter = 0;
+
+            while (n > 0)
+            {
+                counter += (int)(n & 1);
+                n >>= 1;
+            }
+            return counter;
+        }
+    }
+    
+}
