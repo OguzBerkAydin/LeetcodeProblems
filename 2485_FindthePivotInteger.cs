0e292657456c342f40900c037e2f2f589d99c9fb
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-03-16 17:51:04 +0300

    #

---
+++ 2485_FindthePivotInteger.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _2485FindthePivotInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+        public int PivotInteger(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+
+                int left = (i * (i + 1)) / 2;
+                int right = ((n * (n + 1)) / 2) - left + i;
+
+                if (left == right)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+    
+}
